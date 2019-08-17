83af6c69950a9eebd043106bd7d4614e2dede17e
inzynier nierobstwa <klisztwan@gmail.com>
2019-08-17 14:37:30 +0200

    template done, addGame function started

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    function addGame(text){
+        const newGame = document.createElement('li');
+        newGame.classList.add('.container__list--element')//element dodany
+
+        const newGameBar = document.createElement('div');
+        newGameBar.classList.add('.container__list--bar'); //belka
+
+        const newGameDate = document.createElement('div');
+        newGameDate.classList.add
+    }
+
+})
