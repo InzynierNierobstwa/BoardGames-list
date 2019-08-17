f5d2d34fec7e9f1c6d00ea8d04573a054887c74f
inzynier nierobstwa <klisztwan@gmail.com>
2019-08-18 00:06:12 +0200

    gameForm complited, addGame complited

--- app.js
+++ app.js
@@ -3,40 +3,49 @@ document.addEventListener('DOMContentLoaded', function(){
     const gameForm = document.querySelector('.formula--addRemove');
     const gameList = document.querySelector('.container__list--addRemove');
 
+    let gameTable = [];
 
     gameForm.addEventListener('submit', function(e){
         e.preventDefault();
         const textArea = this.querySelector('.formula__input--addRemove');
         if (textArea.value !== ''){
             addGame(textArea.value);
+            addGameTable(textArea.value);
             textArea.value = '';
         }
     })
+
+    function addGameTable(text){
+        gameTable.push(text);
+        console.log(gameTable);
+    }
+
+    //dodawać gry do tablicy i usuwać gry z tablicy, sprawdzać czy gra jest już wpisana
 
     function addGame(text){
         console.log('dodano zadanie');
 
         const newGame = document.createElement('div');
-        newGame.classList.add('.container__list--element')//element dodany
+        newGame.classList.add('container__list--element')//element dodany
 
         const newGameBar = document.createElement('div');
-        newGameBar.classList.add('.container__list--bar'); //belka
+        newGameBar.classList.add('container__list--bar'); //belka
 
         const newGameDate = document.createElement('div');
-        newGameDate.classList.add('.container__list--date');
+        newGameDate.classList.add('container__list--date');
         const date = new Date();
-        const dateText = date.getDate() + '-' + (date.getMonth() + 1) + date.getFullYear() + ' godz. ' + date.getHours() + ':' + date.getMinutes();
-        dateText.innerHTML = newGameDate; //godzina wpierw dodana do zmiennej datetext a potem zamieniona na string
+        const dateText = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' godz. ' + date.getHours() + ':' + date.getMinutes();
+        newGameDate.innerText = dateText; //godzina wpierw dodana do zmiennej datetext a potem zamieniona na string
 
-/*         const deleteGame = document.createElement('div');
+        const deleteGame = document.createElement('div');
         deleteGame.classList.add('.container__list--delete');
-        deleteGame.innerHTML = "<button type='button' name='plusBtn' class='formula__button formula__button--plus'> + </button>"; */
+        deleteGame.innerHTML = "<button type='button' name='minusBtn' class='formula__button formula__button--minus'> X </button>";
 
         newGameBar.appendChild(newGameDate);
-        /* newGameBar.appendChild(deleteGame); */
+        newGameBar.appendChild(deleteGame);
 
         const gameText = document.createElement('div');
-        gameText.classList.add('.container__list--text');
+        gameText.classList.add('container__list--text');
         gameText.innerText = text;
 
         newGame.appendChild(newGameBar);
