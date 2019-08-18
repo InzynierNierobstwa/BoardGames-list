dca173dce29078f609114f03d00d46a873dc168d
inzynier nierobstwa <klisztwan@gmail.com>
2019-08-18 20:23:05 +0200

    links to linkedin and github remove function uploaded

--- app.js
+++ app.js
@@ -8,19 +8,33 @@ document.addEventListener('DOMContentLoaded', function(){
     gameForm.addEventListener('submit', function(e){
         e.preventDefault();
         const textArea = this.querySelector('.formula__input--addRemove');
-        if (textArea.value !== '' && gameTable.indexOf(textArea.value) === -1){
+        if (textArea.value !== '' && gameTable.indexOf(textArea.value.toUpperCase()) === -1){
             addGame(textArea.value);
-            gameTable.push(textArea.value);
+            gameTable.push(textArea.value.toUpperCase());
             textArea.value = '';
             console.log(gameTable);// sprawdza czy nie jest wpisywany pusty element albo już znajdujący się w tabeli
         }
-    })
+    });
+
+    gameList.addEventListener('click', function(e){
+        if(e.target.classList.contains('formula__button--minus')){
+            console.log(word);
+            e.target.closest('.container__list--element').remove();
+            console.log(gameTable);
+        }//usuwa element ze strony i z tablicy pozwalajac na ponowne wpisane go do listy
+    });
+
+    function removeFromArray(array, word){
+        const index = array.findIndex(word);
+        array.splice(index, 1);//usuwanie z tablicy do dopracowania!!!
+    }
+    //funkcja ktora usprawnia szukanie - male/duze litery
 
     function addGame(text){
         console.log('dodano zadanie');
 
         const newGame = document.createElement('div');
-        newGame.classList.add('container__list--element')//element dodany
+        newGame.classList.add('container__list--element');//element dodany
 
         const newGameBar = document.createElement('div');
         newGameBar.classList.add('container__list--bar'); //belka
@@ -31,8 +45,8 @@ document.addEventListener('DOMContentLoaded', function(){
         const dateText = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' godz. ' + date.getHours() + ':' + date.getMinutes();
         newGameDate.innerText = dateText; //godzina wpierw dodana do zmiennej datetext a potem zamieniona na string
 
-        const deleteGame = document.createElement('div');
-        deleteGame.classList.add('.container__list--delete');
+        const deleteGame = document.createElement('button');
+        deleteGame.classList.add('container__list--delete');
         deleteGame.innerHTML = "<button type='button' name='minusBtn' class='formula__button formula__button--minus'> X </button>";
 
         newGameBar.appendChild(newGameDate);
@@ -48,4 +62,4 @@ document.addEventListener('DOMContentLoaded', function(){
         gameList.appendChild(newGame);
     }
 
-})
+});
